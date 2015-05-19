11f5b11fb95e470269edec5489b06b62b5a8de34
RMARomero <romero.ricardo09@gmail.com>
2015-05-19 14:25:45 +0200

    created new views for each controller and modifified ReservationController

--- CampingFlierefluiter/CampingFlierefluiter/Controllers/ReservationController.cs
+++ CampingFlierefluiter/CampingFlierefluiter/Controllers/ReservationController.cs
@@ -8,13 +8,82 @@ namespace CampingFlierefluiter.Controllers
 {
     public class ReservationController : Controller
     {
-        //
-        // GET: /Reservation/
-
+        // GET: Reservation
         public ActionResult Index()
         {
             return View();
         }
 
+        // GET: Reservation/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Reservation/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Reservation/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reservation/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Reservation/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reservation/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Reservation/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
     }
 }
